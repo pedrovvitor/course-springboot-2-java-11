665874c8af918d6ab42eb2ddb26bfa46d2fcc645
Pedro Lima <pedrojppb@gmail.com>
2020-04-02 16:47:42 -0300

    Order, Many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,35 @@
+package com.educandoweb.course.entities.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+public class OrderItemPK implements Serializable {
+	
+	/*
+	 * Esta classe serve para indicar uma chave primaria composta para a classe OrderItem
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+}
