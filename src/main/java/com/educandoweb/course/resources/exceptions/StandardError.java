58efacbc315462b5f51792430633d7167a490e63
Pedro Lima <pedrojppb@gmail.com>
2020-04-03 16:32:49 -0300

    Exception handling update

--- src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -8,14 +8,14 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-	
+
 	public StandardError() {
 		super();
 	}
@@ -68,5 +68,5 @@ public class StandardError implements Serializable {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+
 }
--- src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.educandoweb.course.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public ResourceNotFoundException(Object id) {
 		super("Resource not found for Id " + id);
 	}
